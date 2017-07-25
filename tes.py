678d452f183385a1c181c4711ffa61ffa03ee8a8
root <root@ch2dlfp017.in.ibm.com>
2017-07-25 18:57:35 +0530

    new shell file

--- tes.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python
-print("my  name is bob ")
--- test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-print("Enter your name ")
-with open("/tmp/test","w+") as f:
-     f.write("Testing file from Bobin")
